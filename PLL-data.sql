97ccbbc4e514b005c04da7bdd491e30c7b9fff7f
parkercummins <119359843+parkercummins@users.noreply.github.com>
2024-04-23 20:50:08 -0400

    Update PLL-data.sql
    Added 4 Teams' starters (not run yet so not sure if works)

---
+++ PLL-data.sql
@@ -0,0 +1,56 @@
+INSERT INTO team VALUES ('Waterdogs', 'Philidelphia', 'Bill Tierney', 0, 0);
+INSERT INTO team VALUES ('Cannons', 'Boston', 'Brian Holman', 0, 0);
+INSERT INTO team VALUES ('Whipsnakes', 'Maryland','	Jim Stagnitta', 0, 0);
+INSERT INTO team VALUES ('Atlas', 'New York', 'Mike Pressler', 0, 0);
+INSERT INTO team VALUES ('California', 'Redwoods', 'Nat St. Laurent', 0, 0);
+INSERT INTO team VALUES ('Carolina', 'Chaos', 'Andy Towers', 0, 0);
+INSERT INTO team VALUES ('Denver', 'Outlaws', 'Tim Soudan', 0, 0);
+INSERT INTO team VALUES ('Utah', 'Archers', 'Chris Bates', 0, 0);
+
+/* Waterdogs Starters */
+INSERT INTO player VALUES (1, 'Kieran McArdle', 'Waterdogs', 6, 'Attack', 20, 12, 52500, 'STX');
+INSERT INTO player VALUES (2, 'Michael Sowers', 'Waterdogs', 22, 'Attack', 15, 14, 73000, 'Cascade');
+INSERT INTO player VALUES (3, 'Ethan Walker', 'Waterdogs', 57, 'Attack', 16, 14, 50500, 'Maverick');
+INSERT INTO player VALUES (4, 'Jack Hannah', 'Waterdogs', 3, 'Midfield', 12, 4, 48500, 'Warrior');
+INSERT INTO player VALUES (5, 'Connor Kelly', 'Waterdogs', 40, 'Midfield', 11, 1, 47000, 'Brine');
+INSERT INTO player VALUES (6, 'Jake Carraway', 'Waterdogs', 19, 'Midfield', 12, 3, 46750, 'STX');
+INSERT INTO player VALUES (7, 'Ben Randall', 'Waterdogs', 44, 'Defense', 2, 0, 47500, 'Cascade');
+INSERT INTO player VALUES (8, 'Eli Gobrecht', 'Waterdogs', 25, 'Defense', 2, 0, 46000, 'Warrior');
+INSERT INTO player VALUES (9, 'Liam Byrnes', 'Waterdogs', 29, 'Defense', 0, 2, 52500, 'Brine');
+INSERT INTO player VALUES (10, 'Matt DeLuca', 'Waterdogs', 1, 'Goalie', 0, 0, 61500, 'STX');
+
+/* Cannons Starters */
+INSERT INTO player VALUES (11, 'Marcus Holman', 'Cannons', 1, 'Attack', 30, 13, 74500, 'STX');
+INSERT INTO player VALUES (12, 'Asher Nolting', 'Cannons', 32, 'Attack', 21, 20, 67000, 'Cascade');
+INSERT INTO player VALUES (13, 'Matt Kavanagh', 'Cannons', 50, 'Attack', 14, 10, 58500, 'Maverick');
+INSERT INTO player VALUES (14, 'Ryan Drenner', 'Cannons', 22, 'Midfield', 16, 7, 51500, 'Warrior');
+INSERT INTO player VALUES (15, 'Jonathan Donville', 'Cannons', 33, 'Midfield', 12, 11, 50000, 'Brine');
+INSERT INTO player VALUES (16, 'Chris Aslanian', 'Cannons', 3, 'Midfield', 13, 5, 46000, 'STX');
+INSERT INTO player VALUES (17, 'Jack Kielty', 'Cannons', 40, 'Defense', 0, 0, 53500, 'Cascade');
+INSERT INTO player VALUES (18, 'Max Wayne', 'Cannons', 66, 'Defense', 0, 0, 48000, 'Warrior');
+INSERT INTO player VALUES (19, 'Cade van Raaphorst', 'Cannons', 77, 'Defense', 8, 0, 59500, 'Brine');
+INSERT INTO player VALUES (20, 'Colin Kirst', 'Cannons', 5, 'Goalie', 0, 1, 54500, 'STX');
+
+/* Whipsnakes Starters */
+INSERT INTO player VALUES (21, 'Will Manny', 'Whipsnakes', 4, 'Attack', 15, 16, 66500, 'STX');
+INSERT INTO player VALUES (22, 'Jackson Morrill', 'Whipsnakes', 3, 'Attack', 11, 15, 59500, 'Cascade');
+INSERT INTO player VALUES (23, 'Matt Rambo', 'Whipsnakes', 9, 'Attack', 14, 11, 62750, 'Maverick');
+INSERT INTO player VALUES (24, 'Mike Chanenchuk', 'Whipsnakes', 1, 'Midfield', 17, 7, 56500, 'Warrior');
+INSERT INTO player VALUES (25, 'Tucker Dordevic', 'Whipsnakes', 2, 'Midfield', 19, 2, 53000, 'Brine');
+INSERT INTO player VALUES (26, 'Brad Smith', 'Whipsnakes', 27, 'Midfield', 5, 5, 51500, 'STX');
+INSERT INTO player VALUES (27, 'Bryce Young', 'Whipsnakes', 41, 'Defense', 1, 0, 48000, 'Cascade');
+INSERT INTO player VALUES (28, 'Tim Miller', 'Whipsnakes', 14, 'Defense', 0, 0, 47500, 'Warrior');
+INSERT INTO player VALUES (29, 'Matthew Dunn', 'Whipsnakes', 33, 'Defense', 1, 1, 49500, 'Brine');
+INSERT INTO player VALUES (30, 'Kyle Bernlohr', 'Whipsnakes', 35, 'Goalie', 0, 1, 58500, 'STX');
+
+/* Atlas Starters */
+INSERT INTO player VALUES (31, 'Chris Gray', 'Atlas', 4, 'Attack', 22, 13, 70500, 'STX');
+INSERT INTO player VALUES (32, 'Jeff Teat', 'Atlas', 7, 'Attack', 11, 25, 64500, 'Cascade');
+INSERT INTO player VALUES (33, 'Eric Law', 'Atlas', 15, 'Attack', 22, 5, 55750, 'Maverick');
+INSERT INTO player VALUES (34, 'Bryan Costabile', 'Atlas',26 , 'Midfield', 15, 5, 52000, 'Warrior');
+INSERT INTO player VALUES (35, 'Xander Dickson', 'Atlas', 10, 'Midfield', 14, 6, 49000, 'Brine');
+INSERT INTO player VALUES (36, 'Myles Jones', 'Atlas', 8, 'Midfield', 6, 4, 52000, 'STX');
+INSERT INTO player VALUES (37, 'Gavin Adler', 'Atlas', 88, 'Defense', 1, 0, 47000, 'Cascade');
+INSERT INTO player VALUES (38, 'Tucker Durkin', 'Atlas', 51, 'Defense', 0, 0, 61000, 'Warrior');
+INSERT INTO player VALUES (39, 'Michael Rexrode', 'Atlas', 22, 'Defense', 2, 0, 52000, 'Brine');
+INSERT INTO player VALUES (40, 'Jack Concannon', 'Atlas', 12, 'Goalie', 0, 8, 56250, 'STX');
