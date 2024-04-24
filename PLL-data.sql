283630c9a1df445084e6e658f366872029949238
parkercummins <119359843+parkercummins@users.noreply.github.com>
2024-04-23 23:06:16 -0400

    Update PLL-data.sql
    Includes all INSERTs for Team and Player

--- PLL-data.sql
+++ PLL-data.sql
@@ -53,4 +53,52 @@ INSERT INTO player VALUES (36, 'Myles Jones', 'Atlas', 8, 'Midfield', 6, 4, 5200
 INSERT INTO player VALUES (37, 'Gavin Adler', 'Atlas', 88, 'Defense', 1, 0, 47000, 'Cascade');
 INSERT INTO player VALUES (38, 'Tucker Durkin', 'Atlas', 51, 'Defense', 0, 0, 61000, 'Warrior');
 INSERT INTO player VALUES (39, 'Michael Rexrode', 'Atlas', 22, 'Defense', 2, 0, 52000, 'Brine');
-INSERT INTO player VALUES (40, 'Jack Concannon', 'Atlas', 12, 'Goalie', 0, 8, 56250, 'STX');
+INSERT INTO player VALUES (40, 'Jack Concannon', 'Atlas', 12, 'Goalie', 0, 0, 56250, 'STX');
+
+/* Redwoods Starters */
+INSERT INTO player VALUES (41, 'Ryder Garnsey', 'Redwoods', 50, 'Attack', 25, 11, 68750, 'STX');
+INSERT INTO player VALUES (42, 'Rob Pannell', 'Redwoods', 3, 'Attack', 10, 8, 64500, 'Cascade');
+INSERT INTO player VALUES (43, 'Wes Berg', 'Redwoods', 14, 'Attack', 21, 2, 56000, 'Maverick');
+INSERT INTO player VALUES (44, 'Cole Kirst', 'Redwoods', 47, 'Midfield', 13, 5, 54500, 'Warrior');
+INSERT INTO player VALUES (45, 'Jules Heningburg', 'Redwoods', 7, 'Midfield', 8, 2, 51000, 'Brine');
+INSERT INTO player VALUES (46, 'Romar Dennis', 'Redwoods', 5, 'Midfield', 7, 1, 58000, 'STX');
+INSERT INTO player VALUES (47, 'Garrett Epple', 'Redwoods', 52, 'Defense', 1, 0, 60500, 'Cascade');
+INSERT INTO player VALUES (48, 'Eddy Glazener', 'Redwoods', 30, 'Defense', 1, 0, 48750, 'Warrior');
+INSERT INTO player VALUES (49, 'Arden Cohen', 'Redwoods', 40, 'Defense', 1, 0, 49000, 'Brine');
+INSERT INTO player VALUES (50, 'Jack Kelly', 'Redwoods', 94, 'Goalie', 1, 0, 51000, 'STX');
+
+/* Chaos Starters */
+INSERT INTO player VALUES (51, 'Brian Minicus', 'Chaos', 7, 'Attack', 15, 9, 57000, 'STX');
+INSERT INTO player VALUES (52, 'Josh Byrne', 'Chaos', 22, 'Attack', 15, 6, 62500, 'Cascade');
+INSERT INTO player VALUES (53, 'Tye Kurtz', 'Chaos', 82, 'Attack', 11, 2, 52000, 'Maverick');
+INSERT INTO player VALUES (54, 'Dhane Smith', 'Chaos', 92, 'Midfield', 5, 25, 58500, 'Warrior');
+INSERT INTO player VALUES (55, 'Ryan Smith', 'Chaos', 43, 'Midfield', 14, 4, 52500, 'Brine');
+INSERT INTO player VALUES (56, 'Kyle Jackson', 'Chaos', 47, 'Midfield', 10, 3, 49000, 'STX');
+INSERT INTO player VALUES (57, 'Will Bowen', 'Chaos', 24, 'Defense', 1, 2, 55000, 'Cascade');
+INSERT INTO player VALUES (58, 'Jack Rowlett', 'Chaos', 99, 'Defense', 0, 0, 62000, 'Warrior');
+INSERT INTO player VALUES (59, 'Jarrod Neumann', 'Chaos', 88, 'Defense', 1, 0, 58000, 'Brine');
+INSERT INTO player VALUES (60, 'Blaze Riorden', 'Chaos', 0, 'Goalie', 0, 4, 60000, 'STX');
+
+/* Outlaws Starters */
+INSERT INTO player VALUES (61, 'Logan Wisnauskas', 'Outlaws', 12, 'Attack', 11, 7, 50500, 'STX');
+INSERT INTO player VALUES (62, 'Cross Ferrara', 'Outlaws', 22, 'Attack', 12, 2, 53000, 'Cascade');
+INSERT INTO player VALUES (63, 'Brendan Nichtern', 'Outlaws', 20, 'Attack', 1, 1, 48000, 'Maverick');
+INSERT INTO player VALUES (64, 'Sam Handley', 'Outlaws', 26, 'Midfield', 10, 3, 49000, 'Warrior');
+INSERT INTO player VALUES (65, 'Justin Anderson', 'Outlaws', 21, 'Midfield', 9, 1, 51500, 'Brine');
+INSERT INTO player VALUES (66, 'Jesse King', 'Outlaws', 19, 'Midfield', 3, 5, 50000, 'STX');
+INSERT INTO player VALUES (67, 'Jesse Bernhardt', 'Outlaws', 36, 'Defense', 0, 4, 63000, 'Cascade');
+INSERT INTO player VALUES (68, 'JT Giles-Harris', 'Outlaws', 1, 'Defense', 0, 0, 65750, 'Warrior');
+INSERT INTO player VALUES (69, 'Michael Manley', 'Outlaws', 5, 'Defense', 2, 0, 54000, 'Brine');
+INSERT INTO player VALUES (70, 'Sean Sconone', 'Outlaws', 50, 'Goalie', 0, 0, 52000, 'STX');
+
+/* Archers Starters */
+INSERT INTO player VALUES (71, 'Connor Fields', 'Archers', 5, 'Attack', 28, 11, 65000, 'STX');
+INSERT INTO player VALUES (72, 'Mac OKeefe', 'Archers', 7, 'Attack', 26, 5, 58500, 'Cascade');
+INSERT INTO player VALUES (73, 'Matt Moore', 'Archers', 11, 'Attack', 10, 6, 51000, 'Maverick');
+INSERT INTO player VALUES (74, 'Tom Schreiber', 'Archers', 26, 'Midfield', 18, 17, 49000, 'Warrior');
+INSERT INTO player VALUES (75, 'Tre Leclaire', 'Archers', 44, 'Midfield', 14, 4, 50000, 'Brine');
+INSERT INTO player VALUES (76, 'Grant Ament', 'Archers', 16, 'Midfield', 4, 9, 56000, 'STX');
+INSERT INTO player VALUES (77, 'Graeme Hossack', 'Archers', 81, 'Defense', 0, 1, 49000, 'Cascade');
+INSERT INTO player VALUES (78, 'Warren Jeffrey', 'Archers', 27, 'Defense', 0, 1, 53000, 'Warrior');
+INSERT INTO player VALUES (79, 'Matt McMaon', 'Archers', 77, 'Defense', 0, 0, 50000, 'Brine');
+INSERT INTO player VALUES (80, 'Brett Dobson', 'Archers', 45, 'Goalie', 0, 0, 58000, 'STX');
