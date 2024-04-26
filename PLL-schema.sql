d0341b63e953d7e8222fe9daeb1abfd10d32d0f7
jbaran77 <120224594+jbaran77@users.noreply.github.com>
2024-04-26 09:08:50 -0400

    added play_game procedure

--- PLL-schema.sql
+++ PLL-schema.sql
@@ -58,6 +58,14 @@ CREATE TABLE Player (
 );
 
 
+CREATE TABLE GAMES(
+    home_team VARCHAR2(40),
+    away_team VARCHAR2(40),
+    winner number ,
+    FOREIGN KEY (home_team) REFERENCES Team(Name),
+    FOREIGN KEY (away_team) REFERENCES Team(Name)
+);
+
 
 ALTER TABLE PLAYER
     Add Constraint salary_min Check ( salary >= 25000 );
@@ -98,6 +106,34 @@ BEGIN
     END IF;
 END;
 /
+
+CREATE OR REPLACE PROCEDURE play_game(
+    home_team VARCHAR2,
+    away_team VARCHAR2,
+    winner number
+)IS
+BEGIN
+    INSERT INTO GAMES VALUES (home_team, away_team, winner);
+
+    if(winner = 1) THEN
+        UPDATE Team t
+        SET t.wins = t.wins + 1
+        WHERE t.name = home_team;
+        UPDATE team t
+        SET t.losses = t.losses +1
+        WHERE t.name = away_team;
+    ELSIF (winner = 0) THEN
+        UPDATE Team t
+        SET t.wins = t.wins +1
+        WHERE t.name = away_team;
+        UPDATE team t
+        SET t.losses = t.losses +1
+        WHERE t.name = home_team;
+    END IF;
+    
+END;
+/
+
 
 
 
